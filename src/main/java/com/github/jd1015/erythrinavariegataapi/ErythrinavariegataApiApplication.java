057687c27e85f99936737e137e423ff84b5c4c33
leprashimabuku <shimabuku@lepra.jp>
2018-04-08 17:30:24 +0900

    springinitializrを使ってのプロジェクト作成

---
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/ErythrinavariegataApiApplication.java
@@ -0,0 +1,12 @@
+package com.github.jd1015.erythrinavariegataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErythrinavariegataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErythrinavariegataApiApplication.class, args);
+	}
+}
