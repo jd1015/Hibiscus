93b50e80e904969f39431d124c0279d8f8c34efd
leprashimabuku <shimabuku@lepra.jp>
2018-04-22 17:30:29 +0900

    マッパーアノテーションを追加。

--- src/main/java/com/github/jd1015/erythrinavariegataapi/repository/ThemeRepository.java
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/repository/ThemeRepository.java
@@ -5,12 +5,15 @@ package com.github.jd1015.erythrinavariegataapi.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.github.jd1015.erythrinavariegataapi.model.entity.ThemeEntity;
 
 /**
  * @author lepra
  *
  */
+@Mapper
 public interface ThemeRepository {
 
   Boolean insert(ThemeEntity themeEntity);
