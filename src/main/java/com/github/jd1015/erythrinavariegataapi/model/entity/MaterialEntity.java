0aa2c6583ebf0394a217b2a40b6e8da8920451f5
leprashimabuku <shimabuku@lepra.jp>
2018-05-03 16:15:55 +0900

    materialエンティティの作成

---
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/model/entity/MaterialEntity.java
@@ -0,0 +1,64 @@
+/**
+ *
+ */
+package com.github.jd1015.erythrinavariegataapi.model.entity;
+
+/**
+ * @author lepra
+ *
+ */
+public class MaterialEntity {
+  private Long themeId;
+  private Long materialId;
+  private String title;
+  private String content;
+  /**
+   * @return themeId
+   */
+  public Long getThemeId() {
+    return themeId;
+  }
+  /**
+   * @param themeId セットする themeId
+   */
+  public void setThemeId(Long themeId) {
+    this.themeId = themeId;
+  }
+  /**
+   * @return materialId
+   */
+  public Long getMaterialId() {
+    return materialId;
+  }
+  /**
+   * @param materialId セットする materialId
+   */
+  public void setMaterialId(Long materialId) {
+    this.materialId = materialId;
+  }
+  /**
+   * @return title
+   */
+  public String getTitle() {
+    return title;
+  }
+  /**
+   * @param title セットする title
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  /**
+   * @return content
+   */
+  public String getContent() {
+    return content;
+  }
+  /**
+   * @param content セットする content
+   */
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
