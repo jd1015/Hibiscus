4473b4d4ccd85ea7522e427f0ddffcc8d750fb58
leprashimabuku <shimabuku@lepra.jp>
2018-05-03 17:23:03 +0900

    materialを操作するrepositoryの作成

---
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/repository/MaterialRepository.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package com.github.jd1015.erythrinavariegataapi.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.github.jd1015.erythrinavariegataapi.model.entity.MaterialEntity;
+
+/**
+ * @author lepra
+ *
+ */
+@Mapper
+public interface MaterialRepository {
+
+  Boolean insert(MaterialEntity materialEntity);
+
+  MaterialEntity selectByThemeIdAndMaterialId(Long themeId, Long materialId);
+
+  List<MaterialEntity> selectByThemeId(Long themeId);
+
+  Boolean deleteByThemeIdAndMaterialId(Long thmeId);
+
+  Boolean update(MaterialEntity materialEntity);
+}
