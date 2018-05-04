7606e30f8455ec4ddb061c9eb0abcd71f2e9b2a6
leprashimabuku <shimabuku@lepra.jp>
2018-05-04 16:50:04 +0900

    mybatisで複数引数を渡す場合にxml側で認識できていなかったので修正

--- src/main/java/com/github/jd1015/erythrinavariegataapi/repository/MaterialRepository.java
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/repository/MaterialRepository.java
@@ -6,6 +6,7 @@ package com.github.jd1015.erythrinavariegataapi.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.github.jd1015.erythrinavariegataapi.model.entity.MaterialEntity;
 
@@ -18,11 +19,11 @@ public interface MaterialRepository {
 
   Boolean insert(MaterialEntity materialEntity);
 
-  MaterialEntity selectByThemeIdAndMaterialId(Long themeId, Long materialId);
+  MaterialEntity selectByThemeIdAndMaterialId(@Param("themeId") Long themeId, @Param("materialId") Long materialId);
 
   List<MaterialEntity> selectByThemeId(Long themeId);
 
-  Boolean deleteByThemeIdAndMaterialId(Long themeId, Long materialId);
+  Boolean deleteByThemeIdAndMaterialId(@Param("themeId") Long themeId, @Param("materialId") Long materialId);
 
   Boolean update(MaterialEntity materialEntity);
 }
