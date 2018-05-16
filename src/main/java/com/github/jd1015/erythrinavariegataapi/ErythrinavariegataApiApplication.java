bbc475e0513f81646174f8ed8372a88cdb045905
leprashimabuku <shimabuku@lepra.jp>
2018-05-16 22:27:52 +0900

    cors対策でlocalhostからの接続を許可する

--- src/main/java/com/github/jd1015/erythrinavariegataapi/ErythrinavariegataApiApplication.java
+++ src/main/java/com/github/jd1015/erythrinavariegataapi/ErythrinavariegataApiApplication.java
@@ -2,11 +2,19 @@ package com.github.jd1015.erythrinavariegataapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ErythrinavariegataApiApplication {
+public class ErythrinavariegataApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErythrinavariegataApiApplication.class, args);
 	}
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "GET", "DELETE","POST");
+  }
+
 }
